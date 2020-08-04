d3054f8ab923e2fc42c96187a2a83901386fef74
barosanuemailtest <barosanu.email.test@gmail.com>
2020-08-04 22:41:34 +0300

    5 - Test asertions

--- src/app/Utils.ts
+++ src/app/Utils.ts
@@ -1,8 +1,15 @@
+import { UrlWithParsedQuery, parse } from "url";
 
 
 export class Utils {
 
-    public static toUpperCase(arg: string) {
-        return '';
+
+    public static parseUrl(url: string): UrlWithParsedQuery {
+        return parse(url, true);
+    }
+
+
+    public static toUpperCase(arg: string): string {
+        return arg.toUpperCase();
     }
 }
--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -4,7 +4,25 @@ import { Utils } from '../app/Utils'
 describe('Utils test suite', () => {
 
     test('first test', () => {
-        const abc = Utils.toUpperCase('abc');
-        console.log('tests work!!');
+        const result = Utils.toUpperCase('abc');
+        expect(result).toBe('ABC');
     });
+
+    test('parse simple URL', () => {
+        const parsedUrl = Utils.parseUrl('http://localhost:8080/login');
+        expect(parsedUrl.href).toBe('http://localhost:8080/login');
+        expect(parsedUrl.port).toBe('8080');
+        expect(parsedUrl.protocol).toBe('http:');
+        expect(parsedUrl.query).toEqual({});
+    });
+
+    test('parse URL with query', () => {
+        const parsedUrl = Utils.parseUrl('http://localhost:8080/login?user=user&password=pass');
+        const expectedQuery = {
+            user: 'user',
+            password: 'pass'
+        }
+        expect(parsedUrl.query).toEqual(expectedQuery);
+        expect(expectedQuery).toBe(expectedQuery);
+    })
 });
