306816e98282f1cb7d2ae754c9c755a7076d726e
barosanuemailtest <barosanu.email.test@gmail.com>
2020-09-15 08:54:57 +0300

    26 - Itegration tests setup

--- jest.config.itest.js
+++ jest.config.itest.js
@@ -1,11 +1,10 @@
 module.exports = {
-    roots: ['<rootDir>/src/test'],
+    roots: ['<rootDir>/src/itest'],
     transform: {
         '^.+\\.tsx?$': 'ts-jest'
     },
     testRegex: '(/__test__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     verbose: true,
-    collectCoverage: true,
-    collectCoverageFrom: ['<rootDir>/src/app/**/*.ts']
+    testEnvironment: "node"
 }
---
+++ src/itest/Itest.spec.ts
@@ -0,0 +1,8 @@
+
+
+describe('First itest suite', () => {
+
+    test('initial itest', () => {
+        console.log('initial itest');
+    })
+})
