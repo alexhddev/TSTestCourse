f35c676ec5be62c1858b381859daf890873c2c4f
barosanuemailtest <9tonfr9lkb97A>
2023-01-31 16:50:21 +0200

    Multiple tests structure

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -3,17 +3,52 @@ import { getStringInfo, stringInfo, toUpperCase } from "../app/Utils";
 
 describe('Utils test suite', () => {
     it('should return uppercase', () => {
-
         // arrange
         const expected = 'SOME_NAME';
         const sut = toUpperCase;
-
         // act
         const actual = sut('some_name')
-
         // assert
         expect(actual).toBe(expected);
     });
+
+
+    describe('getStringInfo for My-String should', ()=>{
+        let actual:stringInfo
+
+        beforeEach(()=>{
+            actual = getStringInfo('My-String')
+        });
+
+        test('return right length', ()=>{
+            expect(actual.characters.length).toBe(9)
+        });
+
+        test('return right lowerCase', ()=>{
+            expect(actual.lowerCase).toBe('my-string');
+        });
+
+        test('return right lowerCase', ()=>{
+            expect(actual.upperCase).toBe('MY-STRING');
+        });
+
+        test('return right characters', ()=>{
+            expect(actual.characters).toEqual(['M', 'y', '-','S', 't', 'r','i', 'n', 'g']);
+            expect(actual.characters).toEqual(expect.arrayContaining(['t', 'r','i', 'n', 'g','M', 'y', '-','S']))
+        });
+
+        test('contain M in characters', ()=>{
+            expect(actual.characters).toContain<string>('M');
+        });
+
+        test('return defined extra info', ()=>{
+            expect(actual.extraInfo).toBeDefined();
+        });
+
+        test('return right extra info', ()=>{
+            expect(actual.extraInfo).toEqual({})
+        });
+    })
 
     it('should return info for valid string', () => {
         const actual = getStringInfo('My-String');
@@ -21,11 +56,8 @@ describe('Utils test suite', () => {
         expect(actual.lowerCase).toBe('my-string');
         expect(actual.extraInfo).toEqual({});
 
-        expect(actual.characters.length).toBe(9)
-        expect(actual.characters).toHaveLength(9);
-
         expect(actual.characters).toEqual(['M', 'y', '-','S', 't', 'r','i', 'n', 'g']);
-        expect(actual.characters).toContain<string>('M');
+        
         expect(actual.characters).toEqual(expect.arrayContaining(['t', 'r','i', 'n', 'g','M', 'y', '-','S']))
 
         expect(actual.extraInfo).not.toBe(undefined);
