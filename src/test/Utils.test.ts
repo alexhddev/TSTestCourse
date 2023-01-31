b736bae5c4cd914fc3ad18a8ed342f81d35276b3
barosanuemailtest <barosanu.email.test@gmail.com>
2023-01-31 16:42:41 +0200

    Multiple tests structure

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -12,31 +12,38 @@ describe('Utils test suite', () => {
         expect(actual).toBe(expected);
     })
 
+    describe('getStringInfo for arg My-String should', ()=>{
 
-    it.only('should return info for valid string', ()=>{
-        const actual = getStringInfo('My-String');
+        
+        test('return right length', ()=>{
+            const actual = getStringInfo('My-String');
+            expect(actual.characters).toHaveLength(9);
+        });
+        test('return right lower case', ()=>{
+            const actual = getStringInfo('My-String');
+            expect(actual.lowerCase).toBe('my-string');
+        });
+        test('return right upper case', ()=>{
+            const actual = getStringInfo('My-String');
+            expect(actual.upperCase).toBe('MY-STRING');
+        });
+        test('return right characters', ()=>{
+            const actual = getStringInfo('My-String');
+            expect(actual.characters).toEqual(['M', 'y', '-','S', 't', 'r','i', 'n', 'g']);
+            expect(actual.characters).toContain<string>('M');
+            expect(actual.characters).toEqual(
+                expect.arrayContaining(['S', 't', 'r','i', 'n', 'g', 'M', 'y', '-'])
+            );
+        });
+        test('return defined extra info', ()=>{
+            const actual = getStringInfo('My-String');
+            expect(actual.extraInfo).toBeDefined();
+        });
 
-        expect(actual.lowerCase).toBe('my-string');
-        expect(actual.extraInfo).toEqual({})
-
-        expect(actual.characters.length).toBe(9);
-        expect(actual.characters).toHaveLength(9);
-
-        expect(actual.characters).toEqual(['M', 'y', '-','S', 't', 'r','i', 'n', 'g']);
-        expect(actual.characters).toContain<string>('M');
-        expect(actual.characters).toEqual(
-            expect.arrayContaining(['S', 't', 'r','i', 'n', 'g', 'M', 'y', '-'])
-        );
-
-        expect(actual.extraInfo).not.toBe(undefined);
-        expect(actual.extraInfo).not.toBeUndefined();
-        expect(actual.extraInfo).toBeDefined();
-        expect(actual.extraInfo).toBeTruthy();
-
-
-
-
+        test('return right extra info', ()=>{
+            const actual = getStringInfo('My-String');
+            expect(actual.extraInfo).toEqual({})
+        });
 
     });
-
 });
