c835e06a0ecf2b04a1f4afc25958617d40466603
barosanuemailtest <barosanu.email.test@gmail.com>
2023-02-07 10:17:19 +0200

    Spies

--- src/app/doubles/OtherUtils.ts
+++ src/app/doubles/OtherUtils.ts
@@ -20,3 +20,20 @@ export function toUpperCaseWithCb(arg: string, callBack:LoggerServiceCallBack){
     callBack(`called function with ${arg}`)
     return arg.toUpperCase();
 }
+
+export class OtherStringUtils {
+
+    public callExternalService(){
+        console.log('Calling external service!!!');
+    }
+
+    public toUpperCase(arg: string){
+        return arg.toUpperCase()
+    }
+
+    public logString(arg: string) {
+        console.log(arg);
+    }
+
+
+}
--- src/test/doubles/OtherUtils.test.ts
+++ src/test/doubles/OtherUtils.test.ts
@@ -1,10 +1,38 @@
-import { calculateComplexity, toUpperCaseWithCb } from "../../app/doubles/OtherUtils"
+import { calculateComplexity, OtherStringUtils, toUpperCaseWithCb } from "../../app/doubles/OtherUtils"
 
 
 describe('OtherUtils test suite', ()=>{
 
+    describe.only('OtherStringUtils tests with spies', ()=>{
 
-    describe.only('Tracking callbacks with Jest mocks', ()=>{
+        let sut: OtherStringUtils;
+
+        beforeEach(()=>{
+            sut = new OtherStringUtils();
+        });
+
+        test('Use a spy to track calls', ()=>{
+            const toUpperCaseSpy = jest.spyOn(sut, 'toUpperCase');
+            sut.toUpperCase('asa');
+            expect(toUpperCaseSpy).toBeCalledWith('asa');
+        });
+
+        test('Use a spy to track calls to other module', ()=>{
+            const consoleLogSpy = jest.spyOn(console, 'log');
+            sut.logString('abc');
+            expect(consoleLogSpy).toBeCalledWith('abc');
+        });
+
+        test('Use a spy to replace the implementation of a method', ()=>{
+
+            jest.spyOn(sut, 'callExternalService').mockImplementation(()=>{
+                console.log('calling mocked implementation!!!')
+            });
+            sut.callExternalService();
+        });
+    });
+
+    describe('Tracking callbacks with Jest mocks', ()=>{
 
         const callBackMock = jest.fn();
 
