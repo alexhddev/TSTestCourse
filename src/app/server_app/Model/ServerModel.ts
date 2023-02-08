a0b09013c8e5d258252adcfe68961d887c4b897b
barosanuemailtest <9tonfr9lkb97A>
2023-02-08 16:52:04 +0200

    RegisterHandler

---
+++ src/app/server_app/Model/ServerModel.ts
@@ -0,0 +1,16 @@
+export enum HTTP_CODES {
+    OK = 200,
+    CREATED = 201,
+    BAD_REQUEST = 400,
+    UNAUTHORIZED = 401,
+    NOT_fOUND = 404,
+    INTERNAL_SERVER_ERROR = 500
+}
+
+export enum HTTP_METHODS {
+    GET = 'GET',
+    POST = 'POST',
+    PUT = 'PUT',
+    DELETE = 'DELETE',
+    OPTIONS = 'OPTIONS'
+}
