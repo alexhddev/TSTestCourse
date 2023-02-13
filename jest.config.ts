2231cb158df701d30978db6a458a67d49a3eaaae
barosanuemailtest <9tonfr9lkb97A>
2023-02-13 20:41:24 +0200

    Test wrappers, config to only run tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,15 +1,17 @@
 import type { Config } from '@jest/types';
 
 const baseDir = '<rootDir>/src/app/server_app';
-const baseTestDir = '<rootDir>/src/test/server_app';
+const baseTestDir = '<rootDir>/src/test/server_app2';
 
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   verbose: true,
-  testMatch: [`${baseTestDir}/**/*.ts`],
+  testMatch: [
+    `${baseTestDir}/**/*test.ts`
+  ],
   collectCoverage: true,
-  collectCoverageFrom: [`${baseDir}/**/*.ts`]
+  collectCoverageFrom: [`${baseDir}/**/*.ts`],
 }
 
 export default config;
---
+++ src/test/server_app2/RegisterRequests.test.ts
@@ -0,0 +1,67 @@
+import { Account } from "../../app/server_app/model/AuthModel";
+import { HTTP_METHODS } from "../../app/server_app/model/ServerModel";
+import { Server } from "../../app/server_app/server/Server";
+import { RequestTestWrapper } from "./TestUtils/RequestTestWrapper";
+import { ResponseTestWrapper } from "./TestUtils/ResponseTestWrapper";
+
+
+// const mockInsert = jest.fn();
+// const mockGetBy = jest.fn();
+// const mockUpdate = jest.fn();
+// const mockDelete = jest.fn();
+// const mockGetAllElements = jest.fn();
+
+// jest.mock('../../../app/server_app/data/DataBase', () => {
+//     return {
+//         DataBase: jest.fn().mockImplementation(() => {
+//             return {
+//                 insert: mockInsert,
+//                 getBy: mockGetBy,
+//                 update: mockUpdate,
+//                 delete: mockDelete,
+//                 getAllElements: mockGetAllElements
+//             }
+//         })
+//     }
+// });
+
+const requestWrapper = new RequestTestWrapper();
+const responseWrapper = new ResponseTestWrapper();
+
+const fakeServer = {
+    listen: () => { },
+    close: () => { }
+};
+
+jest.mock('http', () => ({
+    createServer: (cb: any) => {
+        cb(requestWrapper, responseWrapper)
+        return fakeServer;
+    }
+}))
+
+const someAccount: Account = {
+    id: '',
+    password: 'somePassword',
+    userName: 'someUserName'
+}
+
+describe('Register requests', () => {
+    
+    beforeEach(()=>{
+        requestWrapper.headers['user-agent'] = 'jest tests'
+    })
+
+    afterEach(() => {
+        requestWrapper.clearFields();
+        responseWrapper.clearFields();
+    })
+
+    test('should work now', async () => {
+        requestWrapper.method = HTTP_METHODS.POST;
+        requestWrapper.body = someAccount;
+
+        await new Server().startServer();
+    });
+
+});
