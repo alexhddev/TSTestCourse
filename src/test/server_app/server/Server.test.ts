ef5c7e9329224b3407d95a73f986d8c2933e6810
barosanuemailtest <barosanu.email.test@gmail.com>
2023-02-15 14:32:34 +0200

    Server tests 3 - prototype spies

--- src/test/server_app/server/Server.test.ts
+++ src/test/server_app/server/Server.test.ts
@@ -1,5 +1,9 @@
 import { Authorizer } from "../../../app/server_app/auth/Authorizer"
 import { ReservationsDataAccess } from "../../../app/server_app/data/ReservationsDataAccess"
+import { LoginHandler } from "../../../app/server_app/handlers/LoginHandler"
+import { RegisterHandler } from "../../../app/server_app/handlers/RegisterHandler"
+import { ReservationsHandler } from "../../../app/server_app/handlers/ReservationsHandler"
+import { HTTP_CODES } from "../../../app/server_app/model/ServerModel"
 import { Server } from "../../../app/server_app/server/Server"
 
 jest.mock('../../../app/server_app/auth/Authorizer')
@@ -56,6 +60,73 @@ describe('Server test suite', ()=>{
 
         console.log('checking response.end calls:')
         expect(responseMock.end).toBeCalled();
+    })
+
+    it('should handle register requests', async ()=>{
+        requestMock.url = 'localhost:8080/register';
+        const handleRequestSpy = jest.spyOn(RegisterHandler.prototype, 'handleRequest');
+
+        await sut.startServer();
+
+        expect(handleRequestSpy).toBeCalledTimes(1);
+        expect(RegisterHandler).toBeCalledWith(requestMock, responseMock, expect.any(Authorizer));
+    });
+
+    it('should handle login requests', async ()=>{
+        requestMock.url = 'localhost:8080/login';
+        const handleRequestSpy = jest.spyOn(LoginHandler.prototype, 'handleRequest');
+
+        await sut.startServer();
+
+        expect(handleRequestSpy).toBeCalledTimes(1);
+        expect(LoginHandler).toBeCalledWith(requestMock, responseMock, expect.any(Authorizer));
+    });
+
+    it('should handle reservation requests', async ()=>{
+        requestMock.url = 'localhost:8080/reservation';
+        const handleRequestSpy = jest.spyOn(ReservationsHandler.prototype, 'handleRequest');
+
+        await sut.startServer();
+
+        expect(handleRequestSpy).toBeCalledTimes(1);
+        expect(ReservationsHandler).toBeCalledWith(
+            requestMock, 
+            responseMock, 
+            expect.any(Authorizer),
+            expect.any(ReservationsDataAccess));
+    });
+
+    it('should do nothing for not supported routes', async ()=>{
+        requestMock.url = 'localhost:8080/someRandomRoute';
+        const validateTokenSpy = jest.spyOn(Authorizer.prototype, 'validateToken');
+
+        await sut.startServer();
+
+        expect(validateTokenSpy).not.toBeCalled();
+    });
+
+
+    it('should handle errors in serving requests', async ()=>{
+        requestMock.url = 'localhost:8080/reservation';
+        const handleRequestSpy = jest.spyOn(ReservationsHandler.prototype, 'handleRequest');
+        handleRequestSpy.mockRejectedValueOnce(
+            new Error('Some error')
+        )
+
+        await sut.startServer();
+
+        expect(responseMock.writeHead).toBeCalledWith(
+            HTTP_CODES.INTERNAL_SERVER_ERROR, 
+            JSON.stringify(`Internal server error: Some error`)
+        )
+    });
+
+    it('should stop the server if started',async ()=>{
+        await sut.startServer();
+
+        await sut.stopServer();
+
+        expect(serverMock.close).toBeCalledTimes(1);
     })
 
 
