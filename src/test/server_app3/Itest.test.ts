26e24781fb3d4b9503dee8a4b0cc0edec480bfd3
barosanuemailtest <barosanu.email.test@gmail.com>
2023-02-17 13:17:28 +0200

    Writing integration tests

--- src/test/server_app3/Itest.test.ts
+++ src/test/server_app3/Itest.test.ts
@@ -1,4 +1,5 @@
 import { Account } from "../../app/server_app/model/AuthModel";
+import { Reservation } from "../../app/server_app/model/ReservationModel";
 import { HTTP_CODES, HTTP_METHODS } from "../../app/server_app/model/ServerModel";
 import { Server } from "../../app/server_app/server/Server"
 import { makeAwesomeRequest } from "./utils/http-client";
@@ -24,6 +25,14 @@ describe('Server app integration tests', ()=>{
         password: 'somePassword'
     }
 
+    const someReservation: Reservation = {
+        id: '',
+        endDate: 'someEndDate',
+        startDate: 'someStartDate',
+        room: 'someRoom',
+        user: 'someUser'
+    }
+
     it('should register new user', async ()=>{
         const result = await fetch('http://localhost:8080/register', {
             method:HTTP_METHODS.POST,
@@ -45,6 +54,127 @@ describe('Server app integration tests', ()=>{
 
         expect(result.statusCode).toBe(HTTP_CODES.CREATED);
         expect(result.body.userId).toBeDefined();
+    });
+
+    let token: string;
+    it('should login a register user', async ()=>{
+        const result = await fetch('http://localhost:8080/login', {
+            method:HTTP_METHODS.POST,
+            body: JSON.stringify(someUser)
+        });
+        const resultBody = await result.json();
+
+        expect(result.status).toBe(HTTP_CODES.CREATED);
+        expect(resultBody.token).toBeDefined();
+        token = resultBody.token
+    });
+
+    let createdReservationId: string;
+    it('should create reservation if authorized', async ()=>{
+        const result = await fetch('http://localhost:8080/reservation', {
+            method:HTTP_METHODS.POST,
+            body: JSON.stringify(someReservation),
+            headers: {
+                authorization: token
+            }
+        });
+        const resultBody = await result.json();
+
+        expect(result.status).toBe(HTTP_CODES.CREATED);
+        expect(resultBody.reservationId).toBeDefined();
+        createdReservationId = resultBody.reservationId
+    });
+
+    it('should get reservation if authorized', async ()=>{
+        const result = await fetch(`http://localhost:8080/reservation/${createdReservationId}`, {
+            method:HTTP_METHODS.GET,
+            headers: {
+                authorization: token
+            }
+        });
+        const resultBody = await result.json();
+
+        const expectedReservation = structuredClone(someReservation);
+        expectedReservation.id = createdReservationId;
+
+        expect(result.status).toBe(HTTP_CODES.OK);
+        expect(resultBody).toEqual(expectedReservation);
+    });
+
+    it('should create and retrieve multiple reservations if authorized', async ()=>{
+        await fetch('http://localhost:8080/reservation', {
+            method:HTTP_METHODS.POST,
+            body: JSON.stringify(someReservation),
+            headers: {
+                authorization: token
+            }
+        });
+        await fetch('http://localhost:8080/reservation', {
+            method:HTTP_METHODS.POST,
+            body: JSON.stringify(someReservation),
+            headers: {
+                authorization: token
+            }
+        });
+        await fetch('http://localhost:8080/reservation', {
+            method:HTTP_METHODS.POST,
+            body: JSON.stringify(someReservation),
+            headers: {
+                authorization: token
+            }
+        });
+
+        const getAllResult = await fetch(`http://localhost:8080/reservation/all`, {
+            method:HTTP_METHODS.GET,
+            headers: {
+                authorization: token
+            }
+        });
+        const resultBody = await getAllResult.json();
+        expect(getAllResult.status).toBe(HTTP_CODES.OK);
+        expect(resultBody).toHaveLength(4);
+    });
+
+    it('should update reservation if authorized', async ()=>{
+        const updateResult = await fetch(`http://localhost:8080/reservation/${createdReservationId}`, {
+            method:HTTP_METHODS.PUT,
+            body: JSON.stringify({
+                startDate: 'otherStartDate'
+            }),
+            headers: {
+                authorization: token
+            }
+        });
+
+        expect(updateResult.status).toBe(HTTP_CODES.OK);
+
+        const getResult = await fetch(`http://localhost:8080/reservation/${createdReservationId}`, {
+            method:HTTP_METHODS.GET,
+            headers: {
+                authorization: token
+            }
+        });
+        const getRequestBody: Reservation = await getResult.json();
+        expect(getRequestBody.startDate).toBe('otherStartDate');
+    });
+
+    it('should delete reservation if authorized', async ()=>{
+        const deleteResult = await fetch(`http://localhost:8080/reservation/${createdReservationId}`, {
+            method:HTTP_METHODS.DELETE,
+            headers: {
+                authorization: token
+            }
+        });
+
+        expect(deleteResult.status).toBe(HTTP_CODES.OK);
+
+        const getResult = await fetch(`http://localhost:8080/reservation/${createdReservationId}`, {
+            method:HTTP_METHODS.GET,
+            headers: {
+                authorization: token
+            }
+        });
+        expect(getResult.status).toBe(HTTP_CODES.NOT_fOUND);
     });
 
 })
