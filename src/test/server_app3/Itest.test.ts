e3a003a8c1f16824459cf5718bcd378e9ad0e309
barosanuemailtest <barosanu.email.test@gmail.com>
2023-02-20 18:32:12 +0200

    Snapshot testing

--- src/test/server_app3/Itest.test.ts
+++ src/test/server_app3/Itest.test.ts
@@ -1,3 +1,4 @@
+import * as generated from "../../app/server_app/data/IdGenerator";
 import { Account } from "../../app/server_app/model/AuthModel";
 import { Reservation } from "../../app/server_app/model/ReservationModel";
 import { HTTP_CODES, HTTP_METHODS } from "../../app/server_app/model/ServerModel";
@@ -176,5 +177,27 @@ describe('Server app integration tests', ()=>{
         });
         expect(getResult.status).toBe(HTTP_CODES.NOT_fOUND);
     });
+
+    it('snapshot demo', async ()=>{
+        jest.spyOn(generated, 'generateRandomId').mockReturnValueOnce('12345');    
+        await fetch('http://localhost:8080/reservation', {
+            method:HTTP_METHODS.POST,
+            body: JSON.stringify(someReservation),
+            headers: {
+                authorization: token
+            }
+        });
+
+        const getResult = await fetch(`http://localhost:8080/reservation/12345`, {
+            method:HTTP_METHODS.GET,
+            headers: {
+                authorization: token
+            }
+        });
+        const getRequestBody: Reservation = await getResult.json();
+
+        expect(getRequestBody).toMatchSnapshot();
+        expect(getRequestBody).toMatchSnapshot();
+    })
 
 })
