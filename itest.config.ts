e3deae7e2165e489be6a1356defc1184d258084c
barosanuemailtest <barosanu.email.test@gmail.com>
2023-02-20 17:49:06 +0200

    Multiple test configurations

--- itest.config.ts
+++ itest.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from '@jest/types'
 
 const baseDir = '<rootDir>/src/app/server_app';
-const baseTestDir = '<rootDir>/src/test';
+const baseTestDir = '<rootDir>/src/test/server_app3';
 
 const config: Config.InitialOptions = {
     preset: 'ts-jest',
@@ -12,8 +12,10 @@ const config: Config.InitialOptions = {
         `${baseDir}/**/*.ts`
     ],
     testMatch:[
-        `${baseTestDir}/server_app/**/*test.ts`,
-        `${baseTestDir}/server_app2/**/*test.ts`,
+        `${baseTestDir}/**/*test.ts`
+    ],
+    setupFiles: [
+        '<rootDir>/src/test/server_app3/utils/config.ts'
     ]
 }
 
