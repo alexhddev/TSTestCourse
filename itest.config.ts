d10a27ba808c8ca2447721fd0cd535263cf42a9b
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-03-02 14:11:53 +0200

    small fix 2

--- itest.config.ts
+++ itest.config.ts
@@ -4,7 +4,7 @@ const baseDir = '<rootDir>/src/app/server_app';
 const baseTestDir = '<rootDir>/src/test/server_app3';
 
 const config: Config.InitialOptions = {
-    preset: 'ts-jest',  
+    preset: 'ts-jest',
     testEnvironment: 'node',
     verbose: true,
     collectCoverage: true,
