b170ab4425f204bb5ff1dc993f0eb3382b77d9db
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-03-07 16:56:11 +0200

    Query for elements

--- src/LoginComponent.test.tsx
+++ src/LoginComponent.test.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable testing-library/no-node-access */
 import { render, screen } from "@testing-library/react"
 import LoginComponent from "./LoginComponent"
 
@@ -10,14 +11,38 @@ describe('Login component tests', ()=>{
     }
     const setTokenMock = jest.fn();
 
-    it('should render correctly the login component', ()=>{
-       const container =  render(<LoginComponent 
+    let container: HTMLElement;
+
+    function setup() {
+       container = render(<LoginComponent 
             loginService={loginServiceMock}
             setToken={setTokenMock}
             />).container;
-        console.log(container.innerHTML);
+    }
 
+    beforeEach(()=>{
+        setup();
+    })
+
+    it('should render correctly the login component', ()=>{
         const mainElement = screen.getByRole('main');
         expect(mainElement).toBeInTheDocument();
+        expect(screen.queryByTestId('resultLabel')).not.toBeInTheDocument();
+    })
+
+    it('should render correctly - query by test id', ()=>{
+        const inputs = screen.getAllByTestId('input');
+        expect(inputs).toHaveLength(3);
+        expect(inputs[0].getAttribute('value')).toBe('');
+        expect(inputs[1].getAttribute('value')).toBe('');
+        expect(inputs[2].getAttribute('value')).toBe('Login');
+    })
+
+    it('should render correctly - query by document query', ()=>{
+        const inputs = container.querySelectorAll('input');
+        expect(inputs).toHaveLength(3);
+        expect(inputs[0].value).toBe('');
+        expect(inputs[1].value).toBe('');
+        expect(inputs[2].value).toBe('Login');
     })
 })
