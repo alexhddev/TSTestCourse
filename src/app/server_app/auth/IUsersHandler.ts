e254123897b8829423b6b5a8af24551d69f6a230
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-03-07 08:32:32 +0200

    clear all

--- src/app/server_app/auth/IUsersHandler.ts
+++
@@ -1,7 +0,0 @@
-
-
-export interface IUsersHandler {
-    
-    registerUser(userName: string, password: string): Promise<string>;
-    login(userName: string, password: string): Promise<string | undefined>;
-}
--- src/app/server_app/model/ReservationModel.ts
+++
@@ -1,9 +0,0 @@
-
-
-export type Reservation = {
-    id: string,
-    room: string,
-    user: string,
-    startDate: string,
-    endDate: string
-}
