2ab443bb5c659b359d7992ae8ba504794ab76753
alexhddev <21276888+alexhddev@users.noreply.github.com>
2024-01-11 14:48:54 +0200

    Remove disableOptimisticBPs and add Utils.test.ts to testMatch

--- jest.config.ts
+++ jest.config.ts
@@ -14,6 +14,7 @@ const config: Config.InitialOptions = {
     testMatch:[
         `${baseTestDir}/server_app/**/*test.ts`,
         `${baseTestDir}/server_app2/**/*test.ts`,
+        `${baseTestDir}/Utils.test.ts`
     ]
 }
 
